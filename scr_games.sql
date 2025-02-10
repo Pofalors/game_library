9a78b35a69beb93641376d91cb5cb487f10a4fd3
Pofalors <154764200+Pofalors@users.noreply.github.com>
2025-02-10 21:49:16 +0100

    Add files via upload

---
+++ scr_games.sql
@@ -0,0 +1,8 @@
+use myappdb;
+insert into games (user_id,name_of_game,release_date,hours_played) values (2,"Super Mario","2003-03-21",240);
+insert into games (user_id,name_of_game,release_date,hours_played) values (3,"Legends of Zelda","2020-12-05",220);
+insert into games (user_id,name_of_game,release_date,hours_played) values (1,"Pokemon","2007-05-04",300);
+insert into games (user_id,name_of_game,release_date,hours_played) values (5,"Kingdom Hearts","2005-02-21",440);
+insert into games (user_id,name_of_game,release_date,hours_played) values (6,"Final Fantasy VI","2012-02-09",100);
+insert into games (user_id,name_of_game,release_date,hours_played) values (7,"Super Smash Bros","208-12-02",180);
+insert into games (user_id,name_of_game,release_date,hours_played) values (2,"Paper Mario","2010-12-08",360);
---
+++ scr_users.sql
@@ -0,0 +1,9 @@
+create database myappdb;
+use myappdb;
+insert into users (id,name,age) values (1,"Fanis",27);
+insert into users (id,name,age) values (2,"Takis",26);
+insert into users (id,name,age) values (3,"Marina",18);
+insert into users (id,name,age) values (4,"Alexios",20);
+insert into users (id,name,age) values (5,"Eleni",24);
+insert into users (id,name,age) values (6,"Jordan",22);
+insert into users (id,name,age) values (7,"Pantelis",37);
